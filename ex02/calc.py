49a23ebd3277825eb4314a4c63b3435c083aebd1
yukichika kunii <c0a21043f9@edu.teu.ac.jp>
2022-11-29 14:44:46 +0900

    練習２

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,5 +1,14 @@
 import tkinter as tk
-
 root=tk.Tk()
+#練習1
 root.geometry("300x500")
+#練習2
+r,c=0,0
+for num in range(9,-1,-1):
+    button=tk.Button(root,text=f"{num}",width=4,height=2,font=("",30))    
+    button.grid(row=r,column=c)
+    c+=1
+    if c%3==0:
+        r+=1
+        c=0
 root.mainloop()
