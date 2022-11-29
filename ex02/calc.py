7a69348990dc58ef3f200a31d518c4a03533b599
yukichika kunii <c0a21043f9@edu.teu.ac.jp>
2022-11-29 15:30:08 +0900

    練習５

--- ex02/calc.py
+++ ex02/calc.py
@@ -27,4 +27,15 @@ for num in range(9,-1,-1):
 #練習４
 entry=tk.Entry(justify="right",width=10,font=("",40))
 entry.grid(row=0,column=0,columnspan=3)
+
+#練習５
+kigou=["+","="]
+for kg in kigou:
+    button=tk.Button(root,text=f"{kg}",width=4,height=2,font=("",30))
+    button.grid(row=r,column=c)
+    button.bind("<1>",button_click)
+    c+=1
+    if c%3==0:
+        r+=1
+        c=0
 root.mainloop()
