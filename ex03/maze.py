c6f77597177c9960447208e24cfd50cf111e7ebf
yukichika kunii <c0a21043f9@edu.teu.ac.jp>
2022-12-06 14:51:53 +0900

    練習３

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,6 +2,9 @@ import tkinter as tk
 #練習1
 root=tk.Tk()
 root.title("迷えるこうかとん")
+image=tk.PhotoImage(file="fig/3.png")
 canvas=tk.Canvas(width=1500,height=900,bg="black")
+cx,cy=300,400
+canvas.create_image(cx,cy,image=image)
 canvas.pack()
 root.mainloop()
