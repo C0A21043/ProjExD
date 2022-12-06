66ccd62a016e81cfa59a2cfd91a5c63a2e814af0
yukichika kunii <c0a21043f9@edu.teu.ac.jp>
2022-12-06 15:14:03 +0900

    練習５

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,10 +1,18 @@
 import tkinter as tk
-root=tk.Tk()
-root.title("迷えるこうかとん")
-image=tk.PhotoImage(file="fig/3.png")
-canvas=tk.Canvas(width=1500,height=900,bg="black")
-cx,cy=300,400
-canvas.create_image(cx,cy,image=image)
-canvas.pack()
-key=""
-root.mainloop()
+
+def key_down(event):
+    global key
+    key=event.keysym
+    
+
+if __name__ == "__main__":
+    root=tk.Tk()
+    root.title("迷えるこうかとん")
+    image=tk.PhotoImage(file="fig/3.png")
+    canvas=tk.Canvas(width=1500,height=900,bg="black")
+    cx,cy=300,400
+    canvas.create_image(cx,cy,image=image)
+    canvas.pack()
+    key=""
+    root.bind("<KeyPress>",key_down)
+    root.mainloop()
