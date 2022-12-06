5bbddf40e5926dde08f6e43576d739ba6e5d5b88
yukichika kunii <c0a21043f9@edu.teu.ac.jp>
2022-12-06 15:16:15 +0900

    練習６

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,6 +3,10 @@ import tkinter as tk
 def key_down(event):
     global key
     key=event.keysym
+
+def key_up(event):
+    global key
+    key=""
     
 
 if __name__ == "__main__":
@@ -15,4 +19,5 @@ if __name__ == "__main__":
     canvas.pack()
     key=""
     root.bind("<KeyPress>",key_down)
+    root.bind("<KeyRelease>",key_up)
     root.mainloop()
