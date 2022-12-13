aeb7f6493033873ff9d05fcfc63f3603704f7799
yukichika kunii <c0a21043f9@edu.teu.ac.jp>
2022-12-13 14:22:55 +0900

    練習２

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,9 +2,23 @@ import pygame as pg
 import sys
 
 def main():
+    clock=pg.time.Clock()
+    
+
+    
     pg.display.set_caption("逃げろ!こうかとん")
     scrn_sfc=pg.display.set_mode((1600,900)) #Surface
     soto_sfc=pg.image.load("fig/pg_bg.jpg")
+    soto_rct=soto_sfc.get_rect()
+    
+    while True:
+        scrn_sfc.blit(soto_sfc,soto_rct)
+        for event in pg.event.get():
+            if event.type==pg.QUIT:
+                return
+            pg.display.update()
+            clock.tick(1000)
+    
     
 
 
