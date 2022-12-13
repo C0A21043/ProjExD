b68e068cda7ece18ab32e3db1a65c6ffcc45dda0
yukichika kunii <c0a21043f9@edu.teu.ac.jp>
2022-12-13 14:07:11 +0900

    練習１

---
+++ ex04/dodge_bomb.py
@@ -0,0 +1,16 @@
+import pygame as pg 
+import sys
+
+def main():
+    pg.display.set_caption("逃げろ!こうかとん")
+    scrn_sfc=pg.display.set_mode((1600,900)) #Surface
+    soto_sfc=pg.image.load("fig/pg_bg.jpg")
+    
+
+
+if __name__== "__main__":
+    main()
+    pg.init()
+    pg.quit()
+    sys.exit
+
