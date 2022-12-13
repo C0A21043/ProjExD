3ffa69bfa318a63022179efb5d8c88434b86099b
yukichika kunii <c0a21043f9@edu.teu.ac.jp>
2022-12-13 14:30:08 +0900

    練習３

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -7,12 +7,17 @@ def main():
 
     
     pg.display.set_caption("逃げろ!こうかとん")
-    scrn_sfc=pg.display.set_mode((1600,900)) #Surface
+    scrn_sfc=pg.display.set_mode((1600,900)) #Surface(scrn)
     soto_sfc=pg.image.load("fig/pg_bg.jpg")
     soto_rct=soto_sfc.get_rect()
+    tori_sfc=pg.image.load("fig/3.png") #Surface(tori)
+    tori_sfc=pg.transform.rotozoom(tori_sfc,0,2.0)
+    tori_rct=tori_sfc.get_rect()
+    tori_rct.center=900,400
+    tori_sfc.blit(tori_sfc,tori_rct)#blit(tori)
     
     while True:
-        scrn_sfc.blit(soto_sfc,soto_rct)
+        scrn_sfc.blit(soto_sfc,soto_rct)#blit(soto)
         for event in pg.event.get():
             if event.type==pg.QUIT:
                 return
